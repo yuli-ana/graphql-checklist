78d7a1b2dd2a1e9777553decdb6263b418b01e39
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-11 17:57:42 -0400

    Import ApolloProvider +  useQuery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 
+const GET_TODOS = gql`
+query getQuery {
+      todos {
+        done
+        id
+        text
+      }
+    }
+    `;
+
+// add todos
+// toggle todos
+// delete todos
+// list todos
 
 function App() {
-  return ();
+  const { data, loading } = useQuery(GET_TODOS);
+  console.log(data, loading);
+  return (
+    <div>app</div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://intent-raccoon-42.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+});
+
+// client.query({
+//   query: gql`
+//   query getQuery {
+//     todos {
+//       done
+//       id
+//       text
+//     }
+//   }
+//   `
+// }).then(data => console.log(data));
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
