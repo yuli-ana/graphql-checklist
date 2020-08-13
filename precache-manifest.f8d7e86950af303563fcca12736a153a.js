92c9e30d4fca6729b6076a2711f72f4917a7ca73
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-13 18:02:34 -0400

    Updates

---
+++ precache-manifest.f8d7e86950af303563fcca12736a153a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d353770b9c36e4d62e752620b6d63008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef178b0c2aae3128741",
+    "url": "/static/js/2.de450d1c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.de450d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a39b15e75c9977a24c",
+    "url": "/static/js/main.745760e0.chunk.js"
+  },
+  {
+    "revision": "ba459395b22ff9fc20c9",
+    "url": "/static/js/runtime-main.05869889.js"
+  }
+]);
